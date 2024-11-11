e6f3d59cb40a5270ae3fd7d6da8803571be258e9
cejeh001 <cejeh001@gold.ac.uk>
2024-11-11 11:58:31 +0000

    "Updated index.html and script.js: added onclick event to button, renamed id to 'button-container', corrected Math.random() function call, and updated meme path and display logic."

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 // generate random meme function when button is clicked
 
-// get the meme template
-let memeTemplate = document.getElementById('meme');
-
 // button variable
 let button = document.getElementById('button')
 
 function generateMeme() {
-    alert("punani");
     // meme collection
     const folderPath = "memes/";
     const totalMemes = 254;   // number of memes in the folder
 
     // select a random meme
-    let randomIndex = Math.floor(math.random() * totalMemes) + 1;
-    const memePath = `${folderPath}meme${randomIndex}.jpg`;
+    let randomIndex = Math.floor(Math.random() * totalMemes) + 1; // Corrected Math.random()
+    const memePath = `${folderPath}${randomIndex}.jpg`;
 
-    //dislpay the random meme
-    const memeTemplate =  document.getElementById('meme').querySelector("img");
+    // display the random meme
+    const memeTemplate = document.getElementById('meme').querySelector("img");
     memeTemplate.src = memePath;
 }
 
 //link function to generate button
-document.getElementById("button").addEventListener("click", generateMeme);
+button.addEventListener("click", generateMeme);
