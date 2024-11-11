5e15a1cbc21efb59e7b0770d850494b06c2c5267
cejeh001 <cejeh001@gold.ac.uk>
2024-11-11 13:00:06 +0000

    "Updated script.js to select random memes with different file types, removed memes/243.mp4, and modified .DS_Store"

--- script.js
+++ script.js
@@ -3,18 +3,41 @@
 // button variable
 let button = document.getElementById('button')
 
+const memeContainer = document.getElementById("meme")
+
 function generateMeme() {
     // meme collection
     const folderPath = "memes/";
-    const totalMemes = 254;   // number of memes in the folder
+    const totalimages = 254;   // number of memes in the folder
+    const totalGifs = 7
 
-    // select a random meme
-    let randomIndex = Math.floor(Math.random() * totalMemes) + 1; // Corrected Math.random()
-    const memePath = `${folderPath}${randomIndex}.jpg`;
+    let meme;
+
+    // file type
+    const img = 1;
+    const gif = 2;
+
+    // select random type
+    let decidedFileType = Math.floor(Math.random() * 2) + 1;
+
+    if (decidedFileType === img) {
+        let randomIndex = Math.floor(Math.random() * totalImages) + 1; // Corrected Math.random()
+        const memePath = `${folderPath}${randomIndex}.jpg`;
+    }
+
+    else {
+        let randomIndex = Math.floor(Math.random() * totalGifs) + 1; //select between total gifs
+        const memePath = `${folderPath}${randomIndex}.gif`;
+    }
 
     // display the random meme
-    const memeTemplate = document.getElementById('meme').querySelector("img");
-    memeTemplate.src = memePath;
+
+    // if the meme is an image
+    if (decidedFileType === 1){
+        meme = Document.createElement('img')
+        meme.src = memePath;
+    }
+
 }
 
 //link function to generate button
